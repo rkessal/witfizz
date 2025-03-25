03c67dbe060c64f89f2b9ac6c978338a37b852ea
rkessal <kessal.rayhan25@gmail.com>
2025-03-25 22:48:58 +0100

    fix postgres

--- server/src/db/dbConfig.ts
+++ server/src/db/dbConfig.ts
@@ -5,7 +5,11 @@ require("dotenv").config();
 console.log(process.env.DB_PORT);
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  host: process.env.POSTGRES_HOST,
+  port: parseInt(process.env.POSTGRES_PORT || "5432"),
+  database: process.env.POSTGRES_DB,
+  user: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
 });
 
 export default pool;
