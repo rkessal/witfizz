420508cc4c174abd653f7777bc6c471c16cd5928
rkessal <kessal.rayhan25@gmail.com>
2025-03-27 20:26:13 +0100

    fix react app env variable

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,5 +1,5 @@
 //web socket endpoint(our server)
-export const wsEndpoint = process.env.WS_ENDPOINT || 'ws://localhost:5080';
+export const wsEndpoint = process.env.REACT_APP_WS_ENDPOINT || 'ws://localhost:5080';
 
 export const navBarHeight = 0;
 // viewport size (the visible area)
